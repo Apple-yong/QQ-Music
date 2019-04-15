0ec2d5247acac7906a5c90de1dce39e980950aea
Apple-yong <748840808@qq.com>
2019-04-15 12:08:19 +0800

    轮播json数据获取，暂存一版

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var mySwiper = new Swiper('.swiper-container', {
+(function(){
+  var mySwiper = new Swiper('.swiper-container', {
     autoplay: true,//可选选项，自动滑动
     loop : true,
     pagination: {
         el: '.swiper-pagination',
       },
-})
+  })
+})()
--- js/slider.js
+++ js/slider.js
@@ -1,37 +1,45 @@
 (function () {
-    // 轮播图列表渲染
-    var sliderlist = [{
-            src: "images/1234256.jpg",
-        },
-        {
-            src: "images/1234416.jpg",
-        },
-        {
-            src: "images/1234988.jpg",
-        },
-        {
-            src: "images/1234562.jpg",
-        },
-        {
-            src: "images/1222968.jpg",
-        },
-    ]
-    
-    
-    fetch(`/json/rec.json`,{
-        method: 'GET'
-    }).then(res => res.text()).then(
-        data => {
-            text = data
-        }
-    )
-    
+    fetch('/json/rec.json')
+      .then(res => res.json())
+      .then(render)
 
-    var cent = document.querySelector(".swiper-wrapper");
-    for(var i = 0; i < sliderlist.length; i++) {
-    cent.innerHTML += `
-        <div class="swiper-slide"><img src="${sliderlist[i].src}" alt=""></div>
-        `
+    function render(json) {
+      let slides = json.data.slider.map(slide => {
+        return { link: slide.linkUrl, image: slide.picUrl }
+      })
+      var cent = document.querySelector(".swiper-wrapper");
+      for(var i = 0; i < slides.length; i++) {
+      cent.innerHTML += `
+          <div class="swiper-slide"><img src="${slides[i].image}" alt=""></div>
+          `
+      }
     }
+    
+    // 轮播图列表渲染
+    // var sliderlist = [{
+    //         src: "images/1234256.jpg",
+    //     },
+    //     {
+    //         src: "images/1234416.jpg",
+    //     },
+    //     {
+    //         src: "images/1234988.jpg",
+    //     },
+    //     {
+    //         src: "images/1234562.jpg",
+    //     },
+    //     {
+    //         src: "images/1222968.jpg",
+    //     },
+    // ]
+
+    // var cent = document.querySelector(".swiper-wrapper");
+    // for(var i = 0; i < sliderlist.length; i++) {
+    // cent.innerHTML += `
+    //     <div class="swiper-slide"><img src="${sliderlist[i].src}" alt=""></div>
+    //     `
+    // }
+
+    
 })()
 
