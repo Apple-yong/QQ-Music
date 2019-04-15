3e900ad68425bea6a7800a027ce0eddffce1ab6a
Apple-yong <748840808@qq.com>
2019-04-15 16:20:53 +0800

    电台页面前端

--- js/slider.js
+++ js/slider.js
@@ -1,45 +1,10 @@
 (function () {
-    fetch('/json/rec.json')
-      .then(res => res.json())
-      .then(render)
-
-    function render(json) {
-      let slides = json.data.slider.map(slide => {
-        return { link: slide.linkUrl, image: slide.picUrl }
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: true,//可选选项，自动滑动
+        loop : true,
+        pagination: {
+            el: '.swiper-pagination',
+          },
       })
-      var cent = document.querySelector(".swiper-wrapper");
-      for(var i = 0; i < slides.length; i++) {
-      cent.innerHTML += `
-          <div class="swiper-slide"><img src="${slides[i].image}" alt=""></div>
-          `
-      }
-    }
-    
-    // 轮播图列表渲染
-    // var sliderlist = [{
-    //         src: "images/1234256.jpg",
-    //     },
-    //     {
-    //         src: "images/1234416.jpg",
-    //     },
-    //     {
-    //         src: "images/1234988.jpg",
-    //     },
-    //     {
-    //         src: "images/1234562.jpg",
-    //     },
-    //     {
-    //         src: "images/1222968.jpg",
-    //     },
-    // ]
-
-    // var cent = document.querySelector(".swiper-wrapper");
-    // for(var i = 0; i < sliderlist.length; i++) {
-    // cent.innerHTML += `
-    //     <div class="swiper-slide"><img src="${sliderlist[i].src}" alt=""></div>
-    //     `
-    // }
-
-    
 })()
 
